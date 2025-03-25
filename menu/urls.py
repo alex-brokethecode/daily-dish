c151d273ea4cc89dc3e492ffb8aa848d3e9b9d97
alex-brokethecode <alexrtc.dev@gmail.com>
2025-03-25 10:19:06 -0500

    chore: Displaying dish details

--- menu/urls.py
+++ menu/urls.py
@@ -6,4 +6,5 @@ app_name = 'menu'
 
 urlpatterns = [
     path('', views.dish_list, name='dish_list'),
+    path('dish/<int:pk>', views.dish_detail, name='dish_detail')
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Dish
 
 
 def dish_list(request):
     dishes = Dish.objects.all()
+    context = {'dishes': dishes}
+    return render(request=request, template_name='menu/dish_list.html', context=context)
 
-    return render(request, 'menu/dish_list.html', {'dishes': dishes})
+
+def dish_detail(request, pk):
+    dish = get_object_or_404(Dish, id=pk)
+    context = {'dish': dish}
+
+    return render(request=request, template_name='menu/dish_detail.html', context=context)
